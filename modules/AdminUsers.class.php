136916b0f926fe0d15d070f163d0c702adf44972
julian <162964+jbackes@users.noreply.github.com>
2008-09-27 17:18:29 +0000

    fixed small bug for last commit

--- modules/AdminUsers.class.php
+++ modules/AdminUsers.class.php
@@ -217,7 +217,7 @@ class AdminUsers extends ModuleTemplate {
 				$userID = isset($_GET['userID']) ? intval($_GET['userID']) : 0;
 				//$p_ban_nick_email = isset($_POST['p_ban_nick_email']) ? 1 : 0;
 				
-				Functions::getSGValues($_POST['c'], array('deleteUsersPosts','deleteUsersTopics','deleteUsersSentPMs'), 1);
+				$c = Functions::getSGValues($_POST['c'], array('deleteUsersPosts','deleteUsersTopics','deleteUsersSentPMs'), 1);
 
 				if(!$userData = FuncUsers::getUserData($userID)) die('Cannot load data: user '.$userID);
 
