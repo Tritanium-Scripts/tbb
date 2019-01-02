541141474d1485d19d8238e213f1d54aef7d8382
chrissyx <76870771+Chrissyx@users.noreply.github.com>
2019-01-02 15:07:18 +0000

    TBB 1.7: * Updated Smarty to 3.1.33 * Updated copyright year

--- core/Constants.php
+++ core/Constants.php
@@ -3,14 +3,14 @@
  * Defines various constants.
  *
  * @author Christoph Jahn <chris@tritanium-scripts.com>
- * @copyright Copyright (c) 2010-2018 Tritanium Scripts
+ * @copyright Copyright (c) 2010-2019 Tritanium Scripts
  * @license http://creativecommons.org/licenses/by-nc-sa/3.0/ Creative Commons 3.0 by-nc-sa
  * @package TBB1.7
  */
 define('SCRIPTSTART', microtime(true));
 define('INDEXFILE', 'index.php');
 define('ERR_REPORTING', E_ERROR | E_WARNING | E_PARSE); //Report only warnings or higher
-define('COPYRIGHT_YEAR', 2018); //Displayed in page footer
+define('COPYRIGHT_YEAR', 2019); //Displayed in page footer
 /*
  * Two version numbers of this script are used
  * to prevent selective bug using in case of
