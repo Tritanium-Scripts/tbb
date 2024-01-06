bf61ed47458df5436f4b97b239e8f42b880d8fed
Chrissyx <chris@chrissyx.com>
2024-01-06 22:10:38 +0100

    Added documentation

--- core/Interfaces.php
+++ core/Interfaces.php
@@ -5,11 +5,18 @@
 interface PlugIn
 {
     /**
-     * Returns description of this plug-in.
+     * Returns name of author responsible for this plug-in.
      *
-     * @return string Description of plug-in
+     * @return string Author's name
      */
-    public function getDescription(): string;
+    public function getAuthorName(): string;
+
+    /**
+     * Returns URL to website of author responsible for this plug-in.
+     *
+     * @return string Author's homepage or null having none
+     */
+    public function getAuthorUrl(): ?string;
 
     /**
      * Returns name of this plug-in.
@@ -17,6 +24,13 @@ interface PlugIn
      * @return string Name of plug-in
      */
     public function getName(): string;
+
+    /**
+     * Returns description of this plug-in.
+     *
+     * @return string Description of plug-in
+     */
+    public function getDescription(): string;
 
     /**
      * Called on the specified hook. Hooks can be official ones from the system or custom calls e.g. from other plug-ins.
